224e62bf4c7a088958c8f18f2f8fe61b1e7cb3b1
Daniil Voronin <36923175+DanVoron@users.noreply.github.com>
2024-04-17 00:00:01 +0300

    Forps 1.0
    Перемещение по сайту досупно

---
+++ mydjangoproject/static/JS/index.js
@@ -0,0 +1,11 @@
+regPage = document.querySelector(".regPage");
+
+
+
+function Goida(){
+    regPage.style.visibility = "hidden";
+}
+
+function Goida2(){
+    regPage.style.visibility = "visible";
+}
